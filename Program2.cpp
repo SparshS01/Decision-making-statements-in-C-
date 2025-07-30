1a9c6052c3807ca0ab894570075e2ef98139912b
SparshS01 <srivastavasparsh088@gmail.com>
2025-07-31 03:35:46 +0530

    Create Program2.cpp

---
+++ Program2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"enter character"<<endl;
+    cin>>ch;
+    if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    cout<<"Character is a vowel: "<<ch<<endl;
+    else
+    cout<<"Character is not a vowel: "<<ch;
+}
