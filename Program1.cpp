7beb806e6136c3db4e5b37547e8cdd6047e4b0eb
SparshS01 <srivastavasparsh088@gmail.com>
2025-07-31 03:34:50 +0530

    Create Program1.cpp

---
+++ Program1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter_number 1: "<<endl;
+    cin>>a;
+    cout<<"enter_number 2: "<<endl;
+    cin>>b;
+if(a>b)
+cout<<"largest number is: "<<a<<endl;
+else
+cout<<"largest number is: "<<b<<endl;
+}
